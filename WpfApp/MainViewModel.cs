68b0ea1a16ac4d12a673c80f2ccdf3e5a44efb6e
ingridTPires <ingrid.teixeira@trindadetecnologia.com.br>
2024-08-16 17:43:55 -0300

    Implementa grid com filtro auto

---
+++ WpfApp/MainViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace WpfApp
+{
+    internal class MainViewModel
+    {
+        public BindingList<Registro> Registros { get; set; }
+
+        public MainViewModel()
+        {
+            Registros = new BindingList<Registro>()
+            {
+                new Registro() { Nome ="X", Data = DateTime.Now.AddMonths(1).AddMinutes(-10) },
+                new Registro() { Nome ="XX", Data = DateTime.Now.AddMinutes(50) },
+                new Registro() { Nome ="XXX", Data = DateTime.Now }
+            };
+        }
+    }
+    public class Registro
+    {
+        public string Nome { get; set; }
+        public DateTime Data { get; set; }
+    }
+}
